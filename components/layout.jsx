68ca3ee4f27cb7a30f613401534b8c05be603184
Syafiq Hanafee <sy@fiq.me>
2020-08-25 22:56:45 +0800

    fix(Layout): created a const before exporting

--- components/layout.jsx
+++ components/layout.jsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 import { useCurrentUser } from '../lib/hooks';
 
-export default ({ children }) => {
+const Layout = ({ children }) => {
   const [user, { mutate }] = useCurrentUser();
   const handleLogout = async () => {
     await fetch('/api/auth', {
@@ -194,3 +194,5 @@ export default ({ children }) => {
     </>
   );
 };
+
+export default Layout;
